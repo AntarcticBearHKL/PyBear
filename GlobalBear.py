03a03f05f54650573a574b2d846876e31446e76f
a <a@a.com>
2020-03-21 20:18:16 +0900

    'a'

--- GlobalBear.py
+++ GlobalBear.py
@@ -1,46 +1,5 @@
 DebugMode = False
-GlobalAvailabilityCheck = True
-
-Results = {}
-ResultCode = {}
-
-class Result:
-    def __init__(self, *args):
-        if args[0] in ResultCode:
-            self.Value = ResultCode[args[0]][0]
-            self.Explain = ResultCode[args[0]][1]
-            self.HandlerFunction = ResultCode[args[0]][2]
-        else:
-            self.Value = args[0]
-            self.Explain = args[1]
-            self.HandlerFunction = DefaultResultHandlerFunction
-        self.Result = self.Value
-
-    def __str__(self):
-        return self.Explain
-    
-    def ResultWithHandler(self, *args):
-        if len(args)==1:
-            exec('Results[\'' + str(args[0]) + '\'] = self')
-        self.HandlerFunction()
-        return self.Value
-
-    def ResultWithDebug(self):
-        if DebugMode:
-            print('Return Value: ' + self.Value)
-            print('Return Explain: ' + self.Explain)
-            FunctionReturn = self.HandlerFunction()
-            print('HandlerFunction Return: ' + FunctionReturn)
-        return self.Value
-
-def ResultWithFunction(Value, Explain):
-    def RetFunction(Function):
-        Results[Explain] = [Value, Explain, Function]
-    return RetFunction
-
-def DefaultResultHandlerFunction():
-    print('No Result Handler Function Defined')
-
+GlobalAvailabilityCheck = False
 
 class BadBear(Exception):
     def __init__(self, Explain):
@@ -48,3 +7,21 @@ class BadBear(Exception):
     
     def __str__(self):
         return '----------------Error Happend----------------\n' + self.Explain
+
+UserList = {}
+class NewUser:
+    def __init__(self, UserName, Password):
+        self.UserName = UserName
+        self.Password = Password
+        UserList[UserName] = self
+def GetUser(UserName):
+    return UserList[UserName]
+
+ServerList = {}
+class NewServer:
+    def __init__(self, ServerName, IP, Port):
+        self.IP = IP
+        self.Port = Port
+        ServerList[ServerName] = self
+def GetServer(ServerName):
+    return ServerList[ServerName]
--- Library/Data/MySQL.py
+++ Library/Data/MySQL.py
@@ -3,122 +3,129 @@ import os, sys
 
 from PyBear.GlobalBear import *
 
-DatabasesServers = {}
-def NewDatabasesServer(Name, Username, Password, Ip='127.0.0.1', Port=3306):
-    DatabasesServer[Name] = [Username, Password, Ip, Port]
-
-class Database():
-    def __init__(self, ServerName, DatabaseName):
+class DatabaseTable():
+    def __init__(self, ServerName, UserName, DatabaseName, TableName):
         self.Connection = pymysql.connect(
-            host=DatabasesServer[ServerName][2],
-            port=DatabasesServer[ServerName][3],
-            user=DatabasesServer[ServerName][0],
-            passwd=DatabasesServer[ServerName][1],
+            host=GetServer(ServerName).IP,
+            port=GetServer(ServerName).Port,
+            user=GetUser(UserName).UserName,
+            passwd=GetUser(UserName).Password,
             db=DatabaseName,)
+        self.TableName = TableName
         self.Cursor = self.Connection.cursor()
+
+        if self.TableName not in self.__Execute('''SHOW TABLES;''')[0]:
+            SQL = '''CREATE TABLE %s (ID INT AUTO_INCREMENT, PRIMARY KEY(ID)) CHARSET=utf8;''' % (self.TableName)
+            self.__Execute(SQL)
+
+        self.ListColumn()
     
-    def ShowResult(self):
+    def __ShowResult(self):
         return self.Cursor.fetchall()
 
-
-    def NewTable(self, TableName):
-        if self.ListTable().count(TableName) != 0:
-            return False
-        _SQL = '''Create Table %s (id int AUTO_INCREMENT, primary key(id)) charset=utf8;''' % (TableName)
-        self.Cursor.execute(_SQL)
-        self.Connection.commit()
-        return True
-
-    def DropTable(self, TableName):
-        _SQL = '''drop table %s;''' % (TableName)
-        self.Cursor.execute(_SQL)
+    def __Commit(self):
         self.Connection.commit()
 
-    def ListTable(self):
-        _SQL = '''show tables;'''
-        self.Cursor.execute(_SQL)
-        return self.ShowResult()[0]
+    def __Execute(self, SQL):
+        self.Cursor.execute(SQL)
+        self.__Commit()
+        return self.__ShowResult()
 
 
-    def NewColumn(self, TableName, name):
-        if self.ListColumn(TableName).count(name) != 0:
-            return -1
-        _SQL = '''alter table %s add %s char(255);''' % (TableName, name)
-        self.Cursor.execute(_SQL)
-        self.Connection.commit()
-        return 1
+    def AddColumn(self, ColumnName, DataType, Addon='NOT NULL'):
+        if ColumnName in self.Columns:
+            return
 
-    def DropColumn(self, TableName, name):
-        if self.ListColumn(TableName).count(name) == 0:
-            return -1
-        _SQL = '''alter table %s drop %s;''' % (TableName, name)
-        self.Cursor.execute(_SQL)
-        self.Connection.commit()
-        return 1
+        SQL = '''ALTER TABLE %s ADD %s %s %s;''' % (self.TableName, ColumnName, DataType, Addon)
+        self.__Execute(SQL)
 
-    def ListColumn(self, TableName, Raw=False):
-        _SQL = '''show COLUMNS from %s''' % (TableName)
-        self.Cursor.execute(_SQL)
-        if Raw:
-            return self.ShowResult()
-        else:
-            return [Item[0] for Item in self.ShowResult()]
+    def AddIndex(self, ColumnName, IndexName=None):
+        SQL = '''SHOW KEYS FROM %s''' % (self.TableName)
+        self.__Execute(SQL)
+        Indexs = [item[2] for item in self.__ShowResult()]
 
-
-    def Insert(self, TableName, dictionary):
-        self.NewTable(TableName)
-        _field = ''
-        _value = ''
-        _counter = 0
-        for item in dictionary:
-            if _counter != 0:
-                _field += ','
-                _value += ','
-            _field += item
-            _value += ''''%s' ''' % (dictionary[item])
-            _counter += 1
-        for item in _field.split(','):
-            self.NewColumn(TableName, item)      
-        _SQL = '''INSERT INTO %s (%s) VALUES (%s);''' % (TableName, _field, _value)
-        self.Cursor.execute(_SQL)
-        self.Connection.commit()
-    
-    def Delete(self, TableName, id):
-        _SQL = '''DELETE FROM %s WHERE id = %s;''' % (TableName, id)
-        self.Cursor.execute(_SQL)
-        self.Connection.commit()
-
-    def Change(self, TableName, A, B, Column, to):
-        self.NewColumn(TableName, Column)
-        _SQL = '''UPDATE %s SET %s = '%s' WHERE %s = '%s';''' % (TableName, Column, to, A, B)
-        self.Cursor.execute(_SQL)
-        self.Connection.commit()
-
-    def List(self, TableName): 
-        _SQL = '''SELECT * FROM %s''' % (TableName)
-        self.Cursor.execute(_SQL)
-        return self.ShowResult()
-
-    def Search(self, TableName, Src, Des):
-        try:
-            if type(Des) == int:
-                _SQL = '''select * from %s where %s = %s ''' % (TableName, Src, Des)
+        if type(ColumnName) == list:
+            if (IndexName) and (IndexName not in Indexs):
+                ColumnName = ','.join(ColumnName)
+                SQL = '''ALTER TABLE %s ADD INDEX %s(%s)''' % (self.TableName, IndexName, ColumnName)
             else:
-                _SQL = '''select * from %s where %s = '%s' ''' % (TableName, Src, Des)
-            self.Cursor.execute(_SQL)
-            return self.ShowResult()
-        except Exception as e:
-            print('SearchTable Error')
+                return
+        elif type(ColumnName) == str:
+            if ColumnName+'_INDEX' not in Indexs:
+                SQL = '''ALTER TABLE %s ADD INDEX %s(%s)''' % (self.TableName, ColumnName+'_INDEX', ColumnName)
+            else:
+                return
+        self.__Execute(SQL)
+
+    def AddUniqueIndex(self):
+        pass
+
+    def AddFulltextIndex(self):
+        pass
+
+    def ListColumn(self):
+        SQL = '''SHOW COLUMNS FROM %s''' % (self.TableName)
+        self.ColumnsDetail = self.__Execute(SQL)
+        self.Columns = [Item[0] for Item in self.ColumnsDetail]
+        return self.ColumnsDetail
+
+    def ListIndex(self):
+        SQL = '''SHOW KEYS FROM %s''' % (self.TableName)
+        return self.__Execute(SQL)
+
+
+
+    def Insert(self, ColumnName, Data):
+        ColumnName = ','.join(ColumnName)
+
+        for Item in ColumnName:
+            if Item not in self.Columns:
+                return
+
+        for Item in Data:
+            Item = ','.join(['\''+Value+'\'' for Value in Item if type(Value) == str])
+            SQL = '''INSERT INTO %s (%s) VALUES (%s);''' % (self.TableName, ColumnName, Item)
+            self.Cursor.execute(SQL) 
+        self.__Commit()
+    
+    def Delete(self, ID):
+        if type(ID) == list:
+            for Item in ID:
+                self.Cursor.execute('''DELETE FROM %s WHERE ID = %s;''' % (self.TableName, Item))
+        else:
+            self.Cursor.execute('''DELETE FROM %s WHERE ID = %s;''' % (self.TableName, ID))
+        self.__Commit()
+
+    def Change(self, CColumnName, CValue, TColumnName, TValue):
+        if CColumnName not in self.Columns or TColumnName not in self.Columns:
+            return
+
+        if type(CValue) == str:
+            CValue = '\'' + CValue + '\''
+        if type(TValue) == str:
+            TValue = '\'' + TValue + '\''
+        SQL = '''UPDATE %s SET %s = %s WHERE %s = %s;''' % (TableName, TColumnName, TValue, CColumnName, CValue)
+        self.__Execute(SQL)
+
+
+
+    def ListTable(self): 
+        return self.__Execute('''SELECT * FROM %s''' % (self.TableName))
+
+    def SearchTable(self, ColumnName, Value):
+        if ColumnName not in self.Columns:
             return ()
 
+        if type(Value) == int:
+            SQL = '''select * from %s where %s = %s ''' % (TableName, ColumnName, Value)
+        else:
+            SQL = '''select * from %s where %s = '%s' ''' % (TableName, ColumnName, Value)
+        return self.__Execute(SQL)
 
-    def Exec(self, SQL):
-        self.Cursor.execute(_SQL)
-        return self.ShowResult()
+    def SearchID(self, ColumnName, Value):
+        return [Item[0] for Item in self.SearchTable(ColumnName, Value)]
 
-    def Distinct(self, TableName, colunm):
-        _SQL = '''delete %s from %s, (select min(id) as mid, %s from %s group by %s) as t2 where %s.id != t2.mid;''' % (TableName, TableName, colunm, TableName, colunm, TableName)
-        print (_SQL)
-        return
-        self.Cursor.execute(_SQL)
-        self.Connection.commit()
+
+    def Distinct(self, ColumnName):
+        SQL = '''DELETE %s FROM %s, (SELECT min(id) AS mid, %s FROM %s GROUP BY %s) AS t2 WHERE %s.id != t2.mid;''' % (self.TableName, self.TableName, ColumnName, self.TableName, ColumnName, self.TableName)
+        self.__Execute(SQL)
