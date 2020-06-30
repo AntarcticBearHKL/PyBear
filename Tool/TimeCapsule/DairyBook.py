eaec3362f1d9b54341a9952dbaba6a95c500f0cc
a <a@a.com>
2020-06-30 15:53:22 +0800

    'a'

--- Tool/TimeCapsule/DairyBook.py
+++ Tool/TimeCapsule/DairyBook.py
@@ -1,46 +1,49 @@
 import PyBear.GlobalBear as GlobalBear
 import PyBear.Library.Data.MongoDB as MongoDBBear
-import PyBear.Tool.Authentication as Authentication
 import PyBear.Library.Chronus as ChronusBear
 import PyBear.Library.Cipher as CipherBear
 
-def NewEvent(Username, Label, Date, Content, Common, SubLabel): 
-    Table = MongoDBBear.MongoDB(
-        'MongoDB',
-        GlobalBear.TimeCapsuleDatabaseName, 
-        Username)
-    Table.Insert({
-        'Label': Label,
-        'UnionID': CipherBear.NumberIndex(),
-        'Date': str(Date),
-        'Content': Content,
-        'Common': Common,
-        'SubLabel': SubLabel,
-    })
+class DairyBook:
+    def __init__(self, ):
+        pass
 
-def NewPeriod(ServerName, Period, Label, Info, Append):
-    pass
+    def NewEvent(Username, Label, Date, Content, Common, SubLabel): 
+        Table = MongoDBBear.MongoDB(
+            'MongoDB',
+            GlobalBear.TimeCapsuleDatabaseName, 
+            Username)
+        Table.Insert({
+            'Label': Label,
+            'UnionID': CipherBear.NumberIndex(),
+            'Date': str(Date),
+            'Content': Content,
+            'Common': Common,
+            'SubLabel': SubLabel,
+        })
 
-def GetPeriod(Username, Period, Label):
-    Table = MongoDBBear.MongoDB(
-        'MongoDB',
-        GlobalBear.TimeCapsuleDatabaseName, 
-        Username)
-    Condition = {
-        'Label': Label,
-        '$and': [
-            {'Date': {'$gte' : Period[0]}},
-            {'Date': {'$lte' : Period[1]}},
-        ]
-    }
-    return Table.Search(Condition)
+    def NewPeriod(ServerName, Period, Label, Info, Append):
+        pass
 
-def GetDay(Date):
-    Table = MongoDBBear.MongoDB(
-        'MongoDB',
-        GlobalBear.TimeCapsuleDatabaseName, 
-        Username)
-    Condition = {
-        'Date': Date,
-    }
-    return Table.Search(Condition)
+    def GetPeriod(Username, Period, Label):
+        Table = MongoDBBear.MongoDB(
+            'MongoDB',
+            GlobalBear.TimeCapsuleDatabaseName, 
+            Username)
+        Condition = {
+            'Label': Label,
+            '$and': [
+                {'Date': {'$gte' : Period[0]}},
+                {'Date': {'$lte' : Period[1]}},
+            ]
+        }
+        return Table.Search(Condition)
+
+    def GetDay(Date):
+        Table = MongoDBBear.MongoDB(
+            'MongoDB',
+            GlobalBear.TimeCapsuleDatabaseName, 
+            Username)
+        Condition = {
+            'Date': Date,
+        }
+        return Table.Search(Condition)
