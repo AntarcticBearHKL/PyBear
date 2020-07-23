01defd5a830e53a5a0c71341c63d44111230f49d
a <a@a.com>
2020-07-23 17:40:58 +0800

    'a'

--- Utilities/Financial/Quantification.py
+++
@@ -1,9 +0,0 @@
-import talib
-import numpy
-
-import PyBear.GlobalBear as GlobalBear
-import PyBear.Library.Statistics as StatisticsBear
-import PyBear.Utilities.Financial.Market as MarketBear
-
-def GetCor(CodeA, CodeB):
-    pass
